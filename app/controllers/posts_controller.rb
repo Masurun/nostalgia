f162140788e99838210ea8694a017d3df26d5c90
Masurun <wataru.5756@gmail.com>
2021-02-24 02:08:23 +0900

    投稿の詳細、編集、削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,24 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def show
+  end
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+      if @post.destroy
+        redirect_to posts_path
+      end
+  end
   private
   def post_params
     params.require(:post).permit(:content,:title,:image).merge(user_id: current_user.id)
